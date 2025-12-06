21806c706100ec8d3c11a53b030eca58fd8bf7ec
Ramsky1881 <rams.design99@gmail.com>
2025-12-06 17:18:51 +0700

    Update js/blacklist-members.js via Admin Dashboard (Secure)

--- js/blacklist-members.js
+++ js/blacklist-members.js
@@ -1,289 +1,296 @@
 const blacklistMembers = [
-  {
-    nama: "Yuliana",
-    nickname: "-HoliciaX5-",
-    pelanggaran: "Out community",
-    outDate: "2025-09-20",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Samuel",
-    nickname: "Os-SilentX5",
-    pelanggaran: "Out community",
-    outDate: "2025-10-25",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Umam",
-    nickname: "pecel-X5",
-    pelanggaran: "Blacklist penipuan transaksi tidak bayar micash gift",
-    outDate: "2025-06-09",
-    duration: "permanent"
-  },
-  {
-    nama: "Ryan",
-    nickname: "AFK-X5",
-    pelanggaran: "Out community",
-    outDate: "2025-09-24",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Firdan",
-    nickname: "lucieneeX5-yX",
-    pelanggaran: "Out Community",
-    outDate: "2025-09-18",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Aini",
-    nickname: "ImAvriellX5",
-    pelanggaran: "Out Community",
-    outDate: "2025-09-30",
-    duration: 6 // Durasi dalam bulan
-  },
-  {
-    nama: "Ghaffar",
-    nickname: "TjxyfarX5-",
-    pelanggaran: "Out Community",
-    outDate: "2025-10-03",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Ivan",
-    nickname: "Schatzvan-X5",
-    pelanggaran: "Out Community",
-    outDate: "2025-10-03",
-    duration: 6 // Durasi dalam bulan
-  },
-  {
-    nama: "Tasya",
-    nickname: "SYASYA-X5",
-    pelanggaran: "Out Community",
-    outDate: "2025-10-05",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Bobby",
-    nickname: "kwwgX5",
-    pelanggaran: "Out community",
-    outDate: "2025-09-29",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Andika",
-    nickname: "Hxyuth1FcX5-DZ",
-    pelanggaran: "Out community",
-    outDate: "2025-10-06",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "icha",
-    nickname: "xychaX5-",
-    pelanggaran: "Out community",
-    outDate: "2025-10-07",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Rizky",
-    nickname: "SimpleFleuRF-X5",
-    pelanggaran: "Out community",
-    outDate: "2025-10-09",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Devin",
-    nickname: "pynutzX5-xB",
-    pelanggaran: "Out community",
-    outDate: "2025-10-09",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Yulia",
-    nickname: "yuppyAMC-X5 / Babyy-X5",
-    pelanggaran: "Blacklist community karena problem matik",
-    outDate: "2025-10-11",
-    duration: "permanent"
-  },
-  {
-    nama: "hens",
-    nickname: "-hensX5",
-    pelanggaran: "Out community",
-    outDate: "2025-10-11",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Ussy",
-    nickname: "FTrust-X5",
-    pelanggaran: "Out community",
-    outDate: "2025-10-13",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Chandra",
-    nickname: "Chennn-X5",
-    pelanggaran: "Blacklist community karena fake photo",
-    outDate: "2025-10-14",
-    duration: "permanent"
-  },
-  {
-    nama: "Sanny",
-    nickname: "aLx-MatchaX5",
-    pelanggaran: "Out community",
-    outDate: "2025-10-14",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Deswin",
-    nickname: "Wins-X5",
-    pelanggaran: "Out community",
-    outDate: "2025-10-15",
-    duration: 6 // Durasi dalam bulan
-  },
-  {
-    nama: "Fadil",
-    nickname: "StarX51Fc-BTR",
-    pelanggaran: "Out community",
-    outDate: "2025-10-17",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Imary",
-    nickname: "soheeFv-X5",
-    pelanggaran: "Blacklist community (GA NERIMA GAY)",
-    outDate: "2025-10-18",
-    duration: "permanent"
-  },
-  {
-    nama: "HanTan",
-    nickname: "YoSEOBX5",
-    pelanggaran: "Out community",
-    outDate: "2025-10-25",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Cakra",
-    nickname: "oxskyFv-X5",
-    pelanggaran: "Out community",
-    outDate: "2025-10-29",
-    duration: 6 // Durasi dalam bulan
-  },
-  {
-    nama: "Mbew",
-    nickname: "MbeawsL-X5",
-    pelanggaran: "Out community karena",
-    outDate: "2025-10-31",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Renaldo",
-    nickname: "asTDoqiw-X5",
-    pelanggaran: "Out community",
-    outDate: "2025-10-31",
-    duration: 6 // Durasi dalam bulan
-  },
-  {
-    nama: "Alfi",
-    nickname: "YopiewX5-TD",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-11",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Owen",
-    nickname: "HohiX5",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-12",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Ara",
-    nickname: "LovemoreX5-666",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-12",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Ira",
-    nickname: "ScraperX5",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-16",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Tere",
-    nickname: "awTantrum-X5",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-17",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Jov",
-    nickname: "Maive-X5",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-20",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Luki",
-    nickname: "-K141-X5",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-20",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Clay",
-    nickname: "-Druig-X5",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-21",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Maviss",
-    nickname: "DunkinX5-FLz",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-24",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Alam",
-    nickname: "yuji-X5",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-24",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Deni Setiawan",
-    nickname: "TG-ForeverX5",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-24",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Mira",
-    nickname: "ceayX5",
-    pelanggaran: "Out Community",
-    outDate: "2025-11-24",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Pram",
-    nickname: "JeannX5-",
-    pelanggaran: "Out Community",
-    outDate: "2025-12-03",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Okta",
-    nickname: "myAstaroth-X5",
-    pelanggaran: "Out Community",
-    outDate: "2025-12-05",
-    duration: 3 // Durasi dalam bulan
-  },
-  {
-    nama: "Merlin",
-    nickname: "EdenX5-",
-    pelanggaran: "Out Community",
-    outDate: "2025-12-05",
-    duration: 3 // Durasi dalam bulan
-  }
+      {
+            "nama": "Yuliana",
+            "nickname": "-HoliciaX5-",
+            "pelanggaran": "Out community",
+            "outDate": "2025-09-20",
+            "duration": 3
+      },
+      {
+            "nama": "Samuel",
+            "nickname": "Os-SilentX5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-25",
+            "duration": 3
+      },
+      {
+            "nama": "Umam",
+            "nickname": "pecel-X5",
+            "pelanggaran": "Blacklist penipuan transaksi tidak bayar micash gift",
+            "outDate": "2025-06-09",
+            "duration": "permanent"
+      },
+      {
+            "nama": "Ryan",
+            "nickname": "AFK-X5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-09-24",
+            "duration": 3
+      },
+      {
+            "nama": "Firdan",
+            "nickname": "lucieneeX5-yX",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-09-18",
+            "duration": 3
+      },
+      {
+            "nama": "Aini",
+            "nickname": "ImAvriellX5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-09-30",
+            "duration": 6
+      },
+      {
+            "nama": "Ghaffar",
+            "nickname": "TjxyfarX5-",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-10-03",
+            "duration": 3
+      },
+      {
+            "nama": "Ivan",
+            "nickname": "Schatzvan-X5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-10-03",
+            "duration": 6
+      },
+      {
+            "nama": "Tasya",
+            "nickname": "SYASYA-X5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-10-05",
+            "duration": 3
+      },
+      {
+            "nama": "Bobby",
+            "nickname": "kwwgX5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-09-29",
+            "duration": 3
+      },
+      {
+            "nama": "Andika",
+            "nickname": "Hxyuth1FcX5-DZ",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-06",
+            "duration": 3
+      },
+      {
+            "nama": "icha",
+            "nickname": "xychaX5-",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-07",
+            "duration": 3
+      },
+      {
+            "nama": "Rizky",
+            "nickname": "SimpleFleuRF-X5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-09",
+            "duration": 3
+      },
+      {
+            "nama": "Devin",
+            "nickname": "pynutzX5-xB",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-09",
+            "duration": 3
+      },
+      {
+            "nama": "Yulia",
+            "nickname": "yuppyAMC-X5 / Babyy-X5",
+            "pelanggaran": "Blacklist community karena problem matik",
+            "outDate": "2025-10-11",
+            "duration": "permanent"
+      },
+      {
+            "nama": "hens",
+            "nickname": "-hensX5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-11",
+            "duration": 3
+      },
+      {
+            "nama": "Ussy",
+            "nickname": "FTrust-X5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-13",
+            "duration": 3
+      },
+      {
+            "nama": "Chandra",
+            "nickname": "Chennn-X5",
+            "pelanggaran": "Blacklist community karena fake photo",
+            "outDate": "2025-10-14",
+            "duration": "permanent"
+      },
+      {
+            "nama": "Sanny",
+            "nickname": "aLx-MatchaX5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-14",
+            "duration": 3
+      },
+      {
+            "nama": "Deswin",
+            "nickname": "Wins-X5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-15",
+            "duration": 6
+      },
+      {
+            "nama": "Fadil",
+            "nickname": "StarX51Fc-BTR",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-17",
+            "duration": 3
+      },
+      {
+            "nama": "Imary",
+            "nickname": "soheeFv-X5",
+            "pelanggaran": "Blacklist community (GA NERIMA GAY)",
+            "outDate": "2025-10-18",
+            "duration": "permanent"
+      },
+      {
+            "nama": "HanTan",
+            "nickname": "YoSEOBX5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-25",
+            "duration": 3
+      },
+      {
+            "nama": "Cakra",
+            "nickname": "oxskyFv-X5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-29",
+            "duration": 6
+      },
+      {
+            "nama": "Mbew",
+            "nickname": "MbeawsL-X5",
+            "pelanggaran": "Out community karena",
+            "outDate": "2025-10-31",
+            "duration": 3
+      },
+      {
+            "nama": "Renaldo",
+            "nickname": "asTDoqiw-X5",
+            "pelanggaran": "Out community",
+            "outDate": "2025-10-31",
+            "duration": 6
+      },
+      {
+            "nama": "Alfi",
+            "nickname": "YopiewX5-TD",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-11",
+            "duration": 3
+      },
+      {
+            "nama": "Owen",
+            "nickname": "HohiX5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-12",
+            "duration": 3
+      },
+      {
+            "nama": "Ara",
+            "nickname": "LovemoreX5-666",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-12",
+            "duration": 3
+      },
+      {
+            "nama": "Ira",
+            "nickname": "ScraperX5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-16",
+            "duration": 3
+      },
+      {
+            "nama": "Tere",
+            "nickname": "awTantrum-X5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-17",
+            "duration": 3
+      },
+      {
+            "nama": "Jov",
+            "nickname": "Maive-X5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-20",
+            "duration": 3
+      },
+      {
+            "nama": "Luki",
+            "nickname": "-K141-X5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-20",
+            "duration": 3
+      },
+      {
+            "nama": "Clay",
+            "nickname": "-Druig-X5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-21",
+            "duration": 3
+      },
+      {
+            "nama": "Maviss",
+            "nickname": "DunkinX5-FLz",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-24",
+            "duration": 3
+      },
+      {
+            "nama": "Alam",
+            "nickname": "yuji-X5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-24",
+            "duration": 3
+      },
+      {
+            "nama": "Deni Setiawan",
+            "nickname": "TG-ForeverX5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-24",
+            "duration": 3
+      },
+      {
+            "nama": "Mira",
+            "nickname": "ceayX5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-11-24",
+            "duration": 3
+      },
+      {
+            "nama": "Pram",
+            "nickname": "JeannX5-",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-12-03",
+            "duration": 3
+      },
+      {
+            "nama": "Okta",
+            "nickname": "myAstaroth-X5",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-12-05",
+            "duration": 3
+      },
+      {
+            "nama": "Merlin",
+            "nickname": "EdenX5-",
+            "pelanggaran": "Out Community",
+            "outDate": "2025-12-05",
+            "duration": 3
+      },
+      {
+            "nama": "puspachu",
+            "nickname": "yan-yX ",
+            "pelanggaran": "CPL BARU FADEL",
+            "outDate": "2025-12-06",
+            "duration": "permanent"
+      }
 ];
